e5537896a49624b60c6109fd4118265c65d9ff1d
Nabeel Sarguroh <84283811+Scout26@users.noreply.github.com>
2025-03-22 13:03:15 -0600

    4_1.py

--- Viz_Assignment_4_Files/4_1.py
+++ Viz_Assignment_4_Files/4_1.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 np.random.seed(42)
 
 # Question mentions to generate 15 random seed points within the range [0, 19]
-seed_points = np.random.randint(0, 20, size=(15, 2))
+ranseed_pt = np.random.randint(0, 20, size=(15, 2))
 
 # Get data
 vecs = np.reshape(np.fromfile("wind_vectors.raw"), (20,20,2))
@@ -31,7 +31,7 @@ plt.plot(xx, yy, marker='.', color='b', linestyle='none')
 plt.quiver(xx, yy, vecs_flat[:,0], vecs_flat[:,1], width=0.001)
 
 # This part does the plotting of the seed points with graph label for better understanding
-plt.scatter(seed_points[:, 0], seed_points[:, 1], color='red', label='Seed Points')
+plt.scatter(ranseed_pt[:, 0], ranseed_pt[:, 1], color='red', label='Seed Points')
 plt.legend()
 
 plt.show()
